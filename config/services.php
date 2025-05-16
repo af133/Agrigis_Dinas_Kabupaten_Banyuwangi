9c7374255da3be0587f9f112fe828743b6d427e0
Andre Firmansyah <166293262+af133@users.noreply.github.com>
2025-05-16 11:05:29 +0700

    Update services.php

--- config/services.php
+++ config/services.php
@@ -13,7 +13,11 @@ return [
     | a conventional file to locate the various service credentials.
     |
     */
-
+    'cloudinary' => [
+        'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+        'api_key'    => env('CLOUDINARY_API_KEY'),
+        'api_secret' => env('CLOUDINARY_API_SECRET'),
+    ],
     'postmark' => [
         'token' => env('POSTMARK_TOKEN'),
     ],
